3c13a9cb9380b3a4b158e11a448dd5897a1d1c3b
Mike McRoberts <thearduinoguy@users.noreply.github.com>
2015-02-28 09:18:49 +0000

    Update Weather.lua

--- Weather.lua
+++ Weather.lua
@@ -3,9 +3,9 @@
 
 conn=net.createConnection(net.TCP, 0) 
 
--- Put your own API Key in place of the xxxxx's
+-- Once connected to wensite send request
 conn:on("connection",function(conn, payload)
-            conn:send("GET /api/xxxxxxxxxx/conditions/q/CA/San_Francisco.json" 
+            conn:send("GET /api/yourAPIKeyHere/astronomy/q/UK/Gravesend.json"
                         .." HTTP/1.1\r\n" 
                         .."Host: api.wunderground.com\r\n" 
                         .."Connection: close\r\n"
@@ -13,11 +13,46 @@ conn:on("connection",function(conn, payload)
                         .."User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; )\r\n" 
                         .."\r\n"
                         )
-                        end)
-            
+end)
+
+-- What to do once data is received back from the website
 conn:on("receive", function(conn, payload)
-    print(payload)
-    conn:close()
-    end) 
-t = tmr.now()    
+  success = true
+  process(payload)
+  conn:send("ok\r\n")  
+end)
+
+--t = tmr.now()    
 conn:connect(80,'api.wunderground.com') 
+
+function process(payload)
+     s = payload  
+     s=(string.gsub(s, "%c+", "")) -- strip out the newline and carriage return characters
+     print()
+     -- TODO check valid APi return here
+     
+     -- sunrise
+     sunriseHour, sunriseMinute = string.match(s, [["sunrise": {"hour":"(%d+)","minute":"(%d+)"]])
+     print("Sunrise:\t"..sunriseHour..":"..sunriseMinute)
+     -- sunset
+     sunsetHour, sunsetMinute = string.match(s, [["sunset": {"hour":"(%d+)","minute":"(%d+)"]])
+     print("Sunset:\t"..sunsetHour..":"..sunsetMinute)
+
+     -- percentage of moon illuminated
+     percentIlluminated = string.match(s, [["percentIlluminated":"(%d+)"]])
+     print("Percent:\t"..percentIlluminated.."%")
+
+     -- age of moon since last new moon
+     age = string.match(s, [["ageOfMoon":"(%d+)"]])
+     print("Age:\t\t"..age)
+
+     -- Phase of the moon
+     moonPhase = string.match(s, [["phaseofMoon":"(.-)"]])
+     print("Phase:\t"..moonPhase)
+end
+
+
+
+
+
+
